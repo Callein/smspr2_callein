ed05698d52c9155eec5b5c79bb6de0d9b1a4d5d3
callendev20 <callendev20@gmail.com>
2024-07-28 21:49:49 +0900

    :white_check_mark: Implement Tbnotice service

--- src/main/java/com/thc/smspr2/service/impl/TbnoticeServiceImpl.java
+++ src/main/java/com/thc/smspr2/service/impl/TbnoticeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.thc.smspr2.service.impl;
 
 import com.thc.smspr2.domain.Tbnotice;
+import com.thc.smspr2.dto.DefaultDto;
 import com.thc.smspr2.dto.TbnoticeDto;
 import com.thc.smspr2.mapper.TbnoticeMapper;
 import com.thc.smspr2.repository.TbnoticeRepository;
@@ -30,19 +31,74 @@ public class TbnoticeServiceImpl implements TbnoticeService {
     public TbnoticeDto.CreateResDto create(TbnoticeDto.CreateReqDto param){
         return tbnoticeRepository.save(param.toEntity()).toCreateResDto();
     }
+        TbnoticeDto.CreateResDto createResDto = tbnoticeRepository.save(param.toEntity()).toCreateResDto();
+
+        return createResDto;
+    }
+
     @Override
-    public Map<String, Object> detail(Map<String, Object> param){
-        //메퍼에 부탁해보세요!! 2024-08-01 까지!!
-        Map<String, Object> map_tbnotice = tbnoticeMapper.detail(param);
-        return map_tbnotice;
+    public TbnoticeDto.CreateResDto update(TbnoticeDto.UpdateReqDto param){
+        Tbnotice tbnotice = tbnoticeRepository.findById(param.getId()).orElseThrow(()->new RuntimeException("no data"));
+        if(param.getDeleted() != null){
+            tbnotice.setDeleted(param.getDeleted());
+        }
+        if(param.getProcess() != null){
+            tbnotice.setProcess(param.getProcess());
+        }
+
+        if(param.getTitle() != null){
+            tbnotice.setTitle(param.getTitle());
+        }
+        if(param.getContent() != null){
+            tbnotice.setContent(param.getContent());
+        }
+        tbnoticeRepository.save(tbnotice);
+        return tbnotice.toCreateResDto();
+    }
+
+//    @Override
+//    public Map<String, Object> detail(Map<String, Object> param){
+//        //메퍼에 부탁해보세요!! 2024-08-01 까지!!
+//        Map<String, Object> map_tbnotice = tbnoticeMapper.detail(param);
+//        return map_tbnotice;
+//    }
+    @Override
+    public TbnoticeDto.DetailResDto detail(DefaultDto.DetailReqDto param){
+        TbnoticeDto.DetailResDto selectResDto = tbnoticeMapper.detail(param);
+        if(selectResDto == null){ throw new RuntimeException("no data"); }
+
+        return selectResDto;
+    }
+//    @Override
+//    public List<Map<String, Object>> list(Map<String, Object> param){
+//
+//        //검색어도 한번 넣어봅시다.
+//        /*param.put("title", "123");*/
+//
+//        List<Map<String, Object>> list_tbnotice = tbnoticeMapper.list(param);
+//        return list_tbnotice;
+//    }
+
+    @Override
+    public List<TbnoticeDto.DetailResDto> list(TbnoticeDto.ListReqDto param){
+        return detailList(tbnoticeMapper.list(param));
     }
     @Override
-    public List<Map<String, Object>> list(Map<String, Object> param){
-
-        //검색어도 한번 넣어봅시다.
-        /*param.put("title", "123");*/
-
-        List<Map<String, Object>> list_tbnotice = tbnoticeMapper.list(param);
-        return list_tbnotice;
+    public DefaultDto.PagedListResDto pagedList(TbnoticeDto.PagedListReqDto param){
+        int[] returnSize = param.init(tbnoticeMapper.pagedListCount(param));
+        return param.afterBuild(returnSize, detailList(tbnoticeMapper.pagedList(param)));
+    }
+    @Override
+    public List<TbnoticeDto.DetailResDto> scrollList(TbnoticeDto.ScrollListReqDto param){
+        param.init();
+        return detailList(tbnoticeMapper.scrollList(param));
+    }
+    //
+    public List<TbnoticeDto.DetailResDto> detailList(List<TbnoticeDto.DetailResDto> list){
+        List<TbnoticeDto.DetailResDto> newList = new ArrayList<>();
+        for(TbnoticeDto.DetailResDto each : list){
+            newList.add(detail(DefaultDto.DetailReqDto.builder().id(each.getId()).build()));
+        }
+        return newList;
     }
 }
