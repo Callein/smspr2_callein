5791dfb55abc2875383ed1f55cf8e82c02a4d7e8
callendev20 <callendev20@gmail.com>
2024-07-28 14:15:29 +0900

    :white_check_mark: Add Tbnotice entity

--- src/main/java/com/thc/smspr2/domain/Tbnotice.java
+++ src/main/java/com/thc/smspr2/domain/Tbnotice.java
@@ -10,16 +10,18 @@ import lombok.Setter;
 @Entity
 public class Tbnotice extends AuditingFields{
     @Setter @Column(nullable = false, length=400) private String title;
+    @Setter @Column(nullable = false) private String cate;
     @Setter @Column(nullable = true, length=10000) @Lob private String content; // 본문
 
     protected Tbnotice(){}
-    private Tbnotice(String title, String content) {
+    private Tbnotice(String title, String cate, String content) {
         this.title = title;
+        this.cate = cate;
         this.content = content;
     }
 
-    public static Tbnotice of(String title, String content) {
-        return new Tbnotice(title, content);
+    public static Tbnotice of(String title, String cate, String content) {
+        return new Tbnotice(title, cate, content);
     }
 
     public TbnoticeDto.CreateResDto toCreateResDto() {
--- src/main/java/com/thc/smspr2/repository/TbnoticeRepository.java
+++ src/main/java/com/thc/smspr2/repository/TbnoticeRepository.java
@@ -1,7 +1,6 @@
 package com.thc.smspr2.repository;
 
 import com.thc.smspr2.domain.Tbnotice;
-import com.thc.smspr2.domain.Tbpost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
