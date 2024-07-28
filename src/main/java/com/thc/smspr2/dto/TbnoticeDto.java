1cb9cf186f94fc968c2cfb6c6500a9f3261c7e40
callendev20 <callendev20@gmail.com>
2024-07-29 01:37:08 +0900

    ✅ Implement Tbnotice Dto

--- src/main/java/com/thc/smspr2/dto/TbnoticeDto.java
+++ src/main/java/com/thc/smspr2/dto/TbnoticeDto.java
@@ -8,7 +8,6 @@ import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import java.util.List;
 
 public class TbnoticeDto {
 
@@ -19,19 +18,26 @@ public class TbnoticeDto {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class CreateReqDto{
+        @Schema(description = "tbuserId", example="")
+        @NotNull
+        @NotEmpty
+        private String tbuserId;
+
         @Schema(description = "title", example="")
         @NotNull
         @NotEmpty
         @Size(max=400)
         private String title;
-        @Schema(description = "content", example="")
+        @Schema(description = "cate", example="")
         @NotNull
         @NotEmpty
+        private String cate;
+        @Schema(description = "content", example="")
         @Size(max=4000)
         private String content;
 
         public Tbnotice toEntity(){
-            return Tbnotice.of(title, content);
+            return Tbnotice.of(tbuserId, title, cate, content);
         }
     }
     @Builder
@@ -42,6 +48,90 @@ public class TbnoticeDto {
     @NoArgsConstructor
     public static class CreateResDto{
         private String id;
+    }
+    @SuperBuilder
+    @Schema
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UpdateReqDto extends DefaultDto.UpdateReqDto{
+        @Schema(description = "tbuserId", example="")
+        private String tbuserId;
+
+        @Schema(description = "title", example="")
+        @Size(max=400)
+        private String title;
+        @Schema(description = "cate", example="")
+        private String cate;
+        @Schema(description = "content", example="")
+        @Size(max=4000)
+        private String content;
+    }
+
+    @Schema
+    @Getter
+    @Setter
+    public static class DetailResDto extends DefaultDto.DetailResDto{
+        @Schema(description = "title", example="")
+        @Size(max=400)
+        private String title;
+        @Schema(description = "cate", example="")
+        private String cate;
+        @Schema(description = "content", example="")
+        @Size(max=4000)
+        private String content;
+    }
+
+    @SuperBuilder
+    @Schema
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ListReqDto extends DefaultDto.ListReqDto{
+        @Schema(description = "tbuserId", example="")
+        private String tbuserId;
+
+        @Schema(description = "title", example="")
+        @Size(max=400)
+        private String title;
+        @Schema(description = "cate", example="")
+        private String cate;
+    }
+
+    @SuperBuilder
+    @Schema
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class PagedListReqDto extends DefaultDto.PagedListReqDto{
+        @Schema(description = "tbuserId", example="")
+        private String tbuserId;
+
+        @Schema(description = "title", example="")
+        @Size(max=400)
+        private String title;
+        @Schema(description = "cate", example="")
+        private String cate;
+    }
+
+    @SuperBuilder
+    @Schema
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ScrollListReqDto extends DefaultDto.ScrollListReqDto{
+        @Schema(description = "tbuserId", example="")
+        private String tbuserId;
+
+        @Schema(description = "title", example="")
+        @Size(max=400)
+        private String title;
+        @Schema(description = "cate", example="")
+        private String cate;
     }
 
 }
