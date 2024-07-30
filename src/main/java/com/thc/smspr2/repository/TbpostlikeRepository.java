4f26274aad5932c1ab8e8466edbca7f1bd970aad
callendev20 <callendev20@gmail.com>
2024-07-30 14:26:58 +0900

    :white_check_mark: Add Tbpostlike entity, repository

--- src/main/java/com/thc/smspr2/repository/TbpostlikeRepository.java
+++ src/main/java/com/thc/smspr2/repository/TbpostlikeRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TbpostlikeRepository extends JpaRepository<Tbpostlike, String> {
     Tbpostlike findByTbpostIdAndTbuserId(String tbpostId, String tbuserId);
+    boolean existsByTbpostIdAndTbuserId(String tbpostId, String tbuserId);
 }
