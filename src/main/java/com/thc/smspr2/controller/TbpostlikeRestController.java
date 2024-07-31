9af3d70cade600659370d4e7e2c2c5c36e9f127a
callendev20 <callendev20@gmail.com>
2024-07-31 14:44:38 +0900

    ♻️ Clean up TbpostlikeRestController

--- src/main/java/com/thc/smspr2/controller/TbpostlikeRestController.java
+++ src/main/java/com/thc/smspr2/controller/TbpostlikeRestController.java
@@ -19,32 +19,6 @@ public class TbpostlikeRestController {
         this.tbpostlikeService = tbpostlikeService;
     }
 
-
-    /**/
-
-    @Operation(summary = "게시글 좋아요 생성",
-            description = "게시글 좋아요 생성 컨트롤러 <br />"
-                    + "@param TbpostlikeDto.CreateReqDto <br />"
-                    + "@return HttpStatus.CREATED(201) ResponseEntity\\<TbpostlikeDto.CreateResDto\\> <br />"
-                    + "@exception 필수 파라미터 누락하였을 때 등 <br />"
-    )
-    @PostMapping("")
-    public ResponseEntity<TbpostlikeDto.CreateResDto> likePost(@Valid @RequestBody TbpostlikeDto.CreateReqDto param){
-        return ResponseEntity.status(HttpStatus.CREATED).body(tbpostlikeService.likePost(param));
-    }
-
-    @Operation(summary = "게시글 좋아요 취소",
-            description = "게시글 좋아요 취소 컨트롤러 <br />"
-                    + "@param TbpostlikeDto.DeleteReqDto <br />"
-                    + "@return HttpStatus.NO_CONTENT(204) ResponseEntity\\<Void\\> <br />"
-                    + "@exception 필수 파라미터 누락하였을 때 등 <br />"
-    )
-    @DeleteMapping("")
-    public ResponseEntity<Void> unlikePost(@Valid @RequestBody TbpostlikeDto.DeleteReqDto param) {
-        tbpostlikeService.unlikePost(param);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
-
     @Operation(summary = "게시글 좋아요 토글",
             description = "게시글 좋아요 토글 컨트롤러 <br />"
                     + "@param TbpostlikeDto.CreateReqDto <br />"
@@ -52,8 +26,8 @@ public class TbpostlikeRestController {
                     + "@exception 필수 파라미터 누락하였을 때 등 <br />"
     )
     @PostMapping("/toggle")
-    public ResponseEntity<TbpostlikeDto.CreateResDto> toggleLike(@Valid @RequestBody TbpostlikeDto.CreateReqDto param) {
-        TbpostlikeDto.CreateResDto response = tbpostlikeService.toggleLike(param);
+    public ResponseEntity<TbpostlikeDto.ToggleResDto> toggleLike(@Valid @RequestBody TbpostlikeDto.ToggleReqDto param) {
+        TbpostlikeDto.ToggleResDto response = tbpostlikeService.toggleLike(param);
         return ResponseEntity.ok(response);
     }
 }
