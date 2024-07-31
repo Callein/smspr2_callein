b2a777c39d7a935a665b52ecbc4a7488973ca67b
callendev20 <callendev20@gmail.com>
2024-07-31 11:53:45 +0900

    🗃️ Add Unique Constraint to Tbpostlike

--- src/main/java/com/thc/smspr2/domain/Tbpostlike.java
+++ src/main/java/com/thc/smspr2/domain/Tbpostlike.java
@@ -6,21 +6,19 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Table(indexes = {
+@Table(
+        indexes = {
         @Index(columnList = "deleted")
         ,@Index(columnList = "process")
         ,@Index(columnList = "createdAt")
         ,@Index(columnList = "modifiedAt")
-        }
-        ,uniqueConstraints = {
-        @UniqueConstraint(
-                name = "UQ_tbpostlike_tbpost_id_tbuser_id"
-                ,columnNames = {"tbpostId", "tbuserId"}
-        )}
+        },
+        uniqueConstraints = @UniqueConstraint(columnNames = {"tbpostId", "tbuserId"})
 )
 @Entity
 public class Tbpostlike extends AuditingFields {
-    @Setter @Column(nullable = false) private String tbpostId;
+    @Setter
+    @Column(nullable = false) private String tbpostId;
     @Setter @Column(nullable = false) private String tbuserId;
 
     protected Tbpostlike(){}
